929da08e789544b6985a33b6db6fb9b3be08dfb2
Jonathan <jleack@gmail.com>
2018-08-06 15:13:50 -0700

    Added singleton design pattern

--- Module/module.js
+++ Module/module.js
@@ -1,13 +1,13 @@
 /*========================================================================
 // This module design pattern will house a private variable that prevents
-// mutation from global scope. ALong with it are two method closures that
+// mutation from global scope. Along with it are two method closures that
 // can be called to alter the value of the variable.
 ========================================================================*/
 let stockHandler = (() => {
-    // private variable
+    // Private variable
     let inventory = 0;
 
-    // methods for incrementing or decrementing stock
+    // Methods for incrementing or decrementing stock
     return {
         incrementStock: () => {
             return inventory++;
@@ -19,6 +19,6 @@ let stockHandler = (() => {
     };
 })();
 
-// call the module methods to alter the value of inventory
+// Call the module methods to alter the value of inventory
 stockHandler.incrementStock();
 stockHandler.decrementStock();
---
+++ Singleton/singleton.js
@@ -0,0 +1,47 @@
+/*========================================================================
+// This singleton design pattern houses a single instance of a class.
+// When initially instantiated, the singleton will be created. Future
+// calls will reference the already created instance, preventing creation
+// of additional singleton classes in memory.
+========================================================================*/
+let Singleton = (() => {
+    // Store reference to the singleton
+    let instance;
+
+    // The singleton class. We will create a random number within the constructor
+    // to demonstrate that only one singleton is ever created in memory.
+    class Singleton {
+        constructor() {
+            this.firstName = "Singleton";
+            this.lastName = "Example";
+            this.instanceNum = Math.floor(Math.random() * (1000));
+        }
+
+        getSingletonNumber() {
+            return `${this.instanceNum}`;
+        }
+    }
+
+    // Determine if an instance of the singleton already exists. If not, created
+    // one. If so, return reference to the already instantiated singleton.
+    return {
+        getInstance: () => {
+            if (!instance) {
+                instance = new Singleton();           
+            }
+            return instance;
+        }
+   };
+})();
+
+// Instantiate a singleton and print out its randomized instance number
+let singletonInstance = Singleton.getInstance();
+let singletonNumber = singletonInstance.getSingletonNumber();
+console.log(`Singleton instance number: ${singletonNumber}`);
+
+// Attempt to instantiate a second singleton, which will end up referring to
+// the already created singleton instance. Print out its instance number to
+// demonstrate that it's referred to the same instance as above.
+let singletonReference = Singleton.getInstance();
+let singletonReferenceNumber = singletonReference.getSingletonNumber();
+console.log(`Second singleton instance number: ${singletonReferenceNumber}`);
