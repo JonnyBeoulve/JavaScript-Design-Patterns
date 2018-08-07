c3e8ff5197c14cb5177f92107d126019bd11426b
Jonathan <jleack@gmail.com>
2018-08-06 18:44:48 -0700

    Added facade

---
+++ Facade/facade.js
@@ -0,0 +1,16 @@
+/*========================================================================
+// This facade design pattern will check compatibility for an event
+// listener to determine which syntax it should use to comply with the
+// user's browser constraints. No matter which browser the user is using
+// they won't notice that the facade is occurring, hence the name.
+========================================================================*/
+let addEvent = (element, event, fn ) => {
+ 
+    if (element.addEventListener){ 
+        element.addEventListener(event, fn, false ); // Modern browser. False is for a Firefox specific parameter
+    } else if (element.attachEvent){
+        element.attachEvent("on" + event, fn ); // IE and Opera
+    } else {
+        element["on" + event] = fn;
+     }
+ };
