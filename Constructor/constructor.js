389edf99fbe48365f0b02911ed82c1652359b5bb
Jonathan Leack <jonathan.leack@pwc.com>
2019-08-04 17:18:39 -0700

    Linter 2.

--- Constructor/constructor.js
+++ Constructor/constructor.js
@@ -10,37 +10,38 @@
 // class in memory.
 ========================================================================*/
 class Player {
-    constructor() {
-        this.x = 200; // Player location on the x axis
-        this.y = 380; // Player location on the y axis
-        this.spriteIndex = 0; // Index of currently selected sprite
-        this.SPRITE_FILES = [ // Available sprite files
-            'images/char-boy.png',
-            'images/char-cat-girl.png',
-            'images/char-horn-girl.png',
-            'images/char-pink-girl.png',
-            'images/char-princess-girl.png'
-        ]
-        this.sprite = this.SPRITE_FILES[this.spriteIndex]; // Currently selected sprite
-    }
+  constructor() {
+    this.x = 200; // Player location on the x axis
+    this.y = 380; // Player location on the y axis
+    this.spriteIndex = 0; // Index of currently selected sprite
+    this.SPRITE_FILES = [
+      // Available sprite files
+      "images/char-boy.png",
+      "images/char-cat-girl.png",
+      "images/char-horn-girl.png",
+      "images/char-pink-girl.png",
+      "images/char-princess-girl.png"
+    ];
+    this.sprite = this.SPRITE_FILES[this.spriteIndex]; // Currently selected sprite
+  }
 
-    // This function will update the location of the Player character.
-    // If the user is located at the top row of the game field, they score.
-    update() {
-        this.x = this.x;
-        this.y = this.y;
-    
-        if (this.y === -20) {
-            stats.score();
-            stats.timer = stats.timer + 3;
-            alertText("You scored!");
-        }
-    }
+  // This function will update the location of the Player character.
+  // If the user is located at the top row of the game field, they score.
+  update() {
+    this.x = this.x;
+    this.y = this.y;
 
-    // This function will render the Player on the screen.
-    render() {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    if (this.y === -20) {
+      stats.score();
+      stats.timer = stats.timer + 3;
+      alertText("You scored!");
     }
+  }
+
+  // This function will render the Player on the screen.
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
 }
 
 // Instantiate the player object
